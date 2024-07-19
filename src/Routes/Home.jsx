3b77160c0c1808d6a4a10ddb49b233fed5c14bad
ShivaArmada <planckaertg@gmail.com>
2024-07-19 03:57:51 +0200

    mise en place structure et biblio

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,15 @@
+import "../Styles/App.css";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <body>
+
+      </body>
+      
+    </div>
+  );
+}
+
+export default Home;
