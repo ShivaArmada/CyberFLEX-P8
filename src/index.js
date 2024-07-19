618f7978a827f3c94a7b6df0d6881541bbd291aa
ShivaArmada <planckaertg@gmail.com>
2024-07-19 19:00:05 +0200

    Découpage des pages et index basic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Routes/Home';
+import Home from './Routes/Home';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './Routes/About';
+import Works from './Routes/Works';
+import Notfound from './Routes/Notfound';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +14,10 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="*" element={<Notfound />} />
       </Routes>
     </Router>
   </React.StrictMode>
