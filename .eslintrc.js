7ad3df70eaf7d60f59259afda2f1df61cf4d9edc
ShivaArmada <planckaertg@gmail.com>
2024-07-21 16:28:01 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,18 @@ module.exports = {
       'no-sequences': 'off',
       'no-unused-expressions': 'off',
     },
-  };
-  
+    "parserOptions": {
+      "ecmaVersion": 2020,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ],
+    "rules": {
+      // Your custom rules here
+    }
+  }
