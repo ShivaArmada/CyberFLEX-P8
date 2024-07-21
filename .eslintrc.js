04bc7dd07428d17170283255ed828b8c2b7737d4
ShivaArmada <planckaertg@gmail.com>
2024-07-21 16:22:45 +0200

    eslint init

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Autres configurations...
+    rules: {
+      'no-mixed-operators': 'off',
+      'no-sequences': 'off',
+      'no-unused-expressions': 'off',
+    },
+  };
+  
