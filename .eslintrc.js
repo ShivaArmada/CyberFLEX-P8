38fce833edc87de60ab227b169fc6aff37315b57
ShivaArmada <planckaertg@gmail.com>
2024-07-21 16:29:12 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-    // Autres configurations...
-    rules: {
-      'no-mixed-operators': 'off',
-      'no-sequences': 'off',
-      'no-unused-expressions': 'off',
+  // Autres configurations...
+  rules: {
+    'no-mixed-operators': 'off',
+    'no-sequences': 'off',
+    'no-unused-expressions': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    "parserOptions": {
-      "ecmaVersion": 2020,
-      "sourceType": "module",
-      "ecmaFeatures": {
-        "jsx": true
-      }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  // Add your custom rules here
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
     },
-    "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended"
-    ],
-    "rules": {
-      // Your custom rules here
-    }
-  }
+  },
+};
