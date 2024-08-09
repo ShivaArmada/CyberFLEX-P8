f7c804e35b6df9da2041c9e0b197c09f9d5cb560
ShivaArmada <planckaertg@gmail.com>
2024-08-09 19:40:57 +0200

    fix 404

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
-import About from "./Routes/About";
 import Works from "./Routes/Works";
 import Notfound from "./Routes/Notfound";
 import Mentions from "./Routes/Mentions";
-import Contact from "./Routes/Contact";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
@@ -38,10 +36,8 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/works" element={<Works />} />
         <Route path="/mentions" element={<Mentions />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </Router>
