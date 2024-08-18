5c87620f35ee190ad54194947858bffac0df81c9
ShivaArmada <planckaertg@gmail.com>
2024-08-18 21:28:02 +0200

    try fix google tag manager integration

--- src/index.js
+++ src/index.js
@@ -30,14 +30,6 @@ window.axeptioSettings = {
   t.parentNode.insertBefore(e, t);
 })(document, "script");
 
-// Google Tag Manager
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-KH8B3GBQ');
-  // End Google Tag Manager
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
