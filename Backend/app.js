4538253dba997449098014108a46c13db60713ee
ShivaArmada <planckaertg@gmail.com>
2024-08-21 17:06:37 +0200

    upgrade backend 2

--- Backend/app.js
+++ Backend/app.js
@@ -10,4 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Utiliser les routes définies dans routes.js
 app.use('/api', Mailrouter);
 
+// Route de placeholder
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  const message = 'It works!\n';
+  const version = 'NodeJS ' + process.versions.node + '\n';
+  const response = [message, version].join('\n');
+  res.end(response);
+});
+
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,7 @@
-const express = require('express');
-const cors = require('cors');
 const http = require('http');
+const app = require('./app'); // Importez votre app.js
 
-const app = express();
 const port = process.env.PORT || 5000; // Utilisez le port défini dans les variables d'environnement ou 5000 par défaut
-
-// Utilisez le middleware CORS
-app.use(cors());
-
-// Autres middlewares et routes
-app.use(express.json());
-const Mailrouter = require('./Routes/MailRoute'); // Assurez-vous que ce chemin est correct
-app.use('/api', Mailrouter);
-
-// Route de placeholder
-app.get('/', (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  const message = 'It works!\n';
-  const version = 'NodeJS ' + process.versions.node + '\n';
-  const response = [message, version].join('\n');
-  res.end(response);
-});
 
 // Créez le serveur HTTP et intégrez l'application Express
 const server = http.createServer(app);
