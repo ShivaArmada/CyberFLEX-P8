3c0a22b12e39b739ea7a7231cfc84414c9913bf0
ShivaArmada <planckaertg@gmail.com>
2024-08-24 00:38:11 +0200

    suppression du backend (à part)

--- Backend/Routes/MailRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const sendmail = require('../Controllers/MailCTRL'); // Chemin mis à jour
-
-const Mailrouter = express.Router();
-
-Mailrouter.post('/send-email', sendmail.sendEmail); // Utiliser le contrôleur pour envoyer un email
-Mailrouter.post('/subscribe', sendmail.subscribeNewsletter); // Utiliser le contrôleur pour s'inscrire à la newsletter
-
-module.exports = Mailrouter;
