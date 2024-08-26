fb872c5d22235b4ba44c81e4d5e17f91096149f1
ShivaArmada <planckaertg@gmail.com>
2024-08-27 01:13:46 +0200

    com sw

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // public/sw.js
-
+/*
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
@@ -25,7 +25,9 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request).catch(function() {
         console.error('Failed to fetch:', event.request.url);
+        // Retourne une réponse de secours en cas d'échec de récupération
+        return new Response('Service Unavailable', { status: 503, statusText: 'Service Unavailable' });
       });
     })
   );
-});
+}); */
